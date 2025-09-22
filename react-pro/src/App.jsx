c5bbd2351bbd97f05921a2e78227a1b103d2b2c8
Jmkaris <mugwimikariuki101@gmail.com>
2025-09-22 15:38:47 +0300

    feat: implement App and Hello components with basic structure and rendering

--- react-pro/src/App.jsx
+++ react-pro/src/App.jsx
@@ -1,3 +1,13 @@
-function App() {}
+import React from "react";
+import Hello from "./components/Hello";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <p>This is the main App component.</p>
+    </div>
+  );
+}
 
 export default App;
--- react-pro/src/components/Hello.jsx
+++ react-pro/src/components/Hello.jsx
@@ -1,1 +1,10 @@
-function Hello() {}
+import React from "react";
+
+function Hello(props) {
+  console.log(props);
+
+  const name = "karis";
+  return <h1>Hello from a component {props.name} </h1>;
+}
+
+export default Hello;
